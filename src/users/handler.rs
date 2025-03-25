1bfa753c9c60046dd34772f8afca971ce7763e60
agdev04 <dev@agnieve.com>
2025-03-25 22:35:55 +0800

    added me endpoint

--- src/users/handler.rs
+++ src/users/handler.rs
@@ -1,18 +1,22 @@
-use actix_web::{HttpMessage, HttpRequest};
+use crate::{
+    db::establish_connection,
+    schema::users,
+    users::model::{NewUser, UpdateUser, User},
+};
 use actix_web::{web, HttpResponse, Result};
-use argon2::Argon2;
-use argon2::password_hash::SaltString;
+use actix_web::{HttpMessage, HttpRequest};
 use argon2::password_hash::rand_core::OsRng;
+use argon2::password_hash::SaltString;
+use argon2::Argon2;
+use argon2::PasswordHasher;
+use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
-use crate::{db::establish_connection, users::model::{NewUser, UpdateUser, User}, schema::users};
-use diesel::prelude::*;
-use argon2::PasswordHasher;
 
 #[derive(Serialize)]
 pub struct GenericResponse {
-  pub status: String,
-  pub message: String,
+    pub status: String,
+    pub message: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -31,20 +35,21 @@ pub struct LoginUser {
 pub fn hash_password(password: String) -> Result<String, actix_web::Error> {
     let salt = SaltString::generate(&mut OsRng);
     let argon2 = Argon2::default();
-  
-    let hash_result = argon2.hash_password(password.as_bytes(), &salt)
+
+    let hash_result = argon2
+        .hash_password(password.as_bytes(), &salt)
         .map_err(|e| actix_web::error::ErrorInternalServerError(e))?;
     Ok(hash_result.to_string())
-  }
+}
 
 pub async fn create_user(new_user: web::Json<NewUser>) -> Result<HttpResponse> {
-  let mut connection = establish_connection();
-		
+    let mut connection = establish_connection();
+
     let hashed_password = hash_password(new_user.password.clone()).unwrap();
     let new_user = NewUser {
         password: hashed_password.to_string(),
         ..new_user.into_inner()
-    };  
+    };
 
     diesel::insert_into(users::table)
         .values(&new_user)
@@ -54,13 +59,16 @@ pub async fn create_user(new_user: web::Json<NewUser>) -> Result<HttpResponse> {
 }
 
 pub async fn get_users(req: HttpRequest) -> Result<HttpResponse> {
-
     let connection = &mut establish_connection();
     let results = users::table
         .load::<User>(connection)
         .map_err(|e| actix_web::error::ErrorInternalServerError(e))?;
 
-    let user_id = req.extensions().get::<String>().cloned().unwrap_or_default();
+    let user_id = req
+        .extensions()
+        .get::<String>()
+        .cloned()
+        .unwrap_or_default();
 
     Ok(HttpResponse::Ok().json(json!({
         "status": "success",
@@ -100,21 +108,36 @@ pub async fn update_user(id: web::Path<i32>, user: web::Json<UpdateUser>) -> Res
                     message: "User not found".to_string(),
                 }))
             }
-        },
+        }
         Err(_) => Ok(HttpResponse::InternalServerError().json(GenericResponse {
             status: "error".to_string(),
             message: "Failed to update user".to_string(),
-        }))
+        })),
     }
 }
-        
 
+pub async fn get_user_by_token(req: HttpRequest) -> Result<HttpResponse> {
+    let mut connection = establish_connection();
+    let user_id = req
+        .extensions()
+        .get::<String>()
+        .cloned()
+        .unwrap_or_default()
+        .parse::<i32>()
+        .map_err(|_| actix_web::error::ErrorUnauthorized("Invalid token"))?;
+
+    let result = users::table
+        .find(user_id)
+        .first::<User>(&mut connection)
+        .map_err(|e| actix_web::error::ErrorInternalServerError(e))?;
+
+    Ok(HttpResponse::Ok().json(result))
+}
 
 pub async fn delete_user(id: web::Path<i32>) -> Result<HttpResponse> {
     let mut connection = establish_connection();
     let user_id = id.into_inner();
-    let result = diesel::delete(users::table.find(user_id))
-        .execute(&mut connection);
+    let result = diesel::delete(users::table.find(user_id)).execute(&mut connection);
 
     match result {
         Ok(num_deleted) => {
@@ -129,10 +152,10 @@ pub async fn delete_user(id: web::Path<i32>) -> Result<HttpResponse> {
                     message: "User not found".to_string(),
                 }))
             }
-        },
+        }
         Err(_) => Ok(HttpResponse::InternalServerError().json(GenericResponse {
             status: "error".to_string(),
             message: "Failed to delete user".to_string(),
-        }))
+        })),
     }
 }
--- src/users/route.rs
+++ src/users/route.rs
@@ -1,15 +1,16 @@
-
+use crate::users::handler::{create_user, delete_user, get_user, get_users, update_user};
 use actix_web::web;
-use crate::users::handler::{create_user, delete_user, get_user, get_users, update_user} ;
 
+use super::handler::get_user_by_token;
 
 pub fn user_config(cfg: &mut web::ServiceConfig) {
     cfg.service(
-    web::scope("/users")
-      .route("", web::get().to(get_users))
-      .route("", web::post().to(create_user))
-      .route("/{id}", web::get().to(get_user))
-      .route("/{id}", web::put().to(update_user))
-      .route("/{id}", web::delete().to(delete_user))
-  );
+        web::scope("/users")
+            .route("", web::get().to(get_users))
+            .route("", web::post().to(create_user))
+            .route("/{id}", web::get().to(get_user))
+            .route("/{id}", web::put().to(update_user))
+            .route("/{id}", web::delete().to(delete_user))
+            .route("/my/data", web::get().to(get_user_by_token)),
+    );
 }
